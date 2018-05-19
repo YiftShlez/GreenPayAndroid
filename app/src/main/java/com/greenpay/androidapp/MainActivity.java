e44035e53b626d62cf9d11832ecc4eb1eefb00f6
Yiftah <yift.shl2@gmail.com>
2018-05-19 18:59:38 +0300

    App change: scan barcode instead of detect wifi added functions to detect barcode using camera added connection with server

--- app/src/main/java/com/greenpay/androidapp/MainActivity.java
+++ app/src/main/java/com/greenpay/androidapp/MainActivity.java
@@ -1,129 +1,275 @@
 package com.greenpay.androidapp;
 
-import android.content.BroadcastReceiver;
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * The main activity of the app
- * shows a list of the stores that support GreenPay in your area
- * clicking a store will take you to an activity where you can pay in the store
+ * The activity scans qr code and sends it to the server
  *
  * @author Yiftah Schlesinger
  * @version 1.0
  */
 public class MainActivity extends AppCompatActivity
 {
-    ListView wifilist = null; //a listview to display available stores
-    TextView stateText; //a textview to display app state
-    BroadcastReceiver wifiReceiver; //a receiver to detect new WiFi networks
-    boolean wifiStateOriginal; //the original state of the WiFi (on/off)
-    WifiManager mainWiFi; //The object that controls the WiFi
+
+    static final String TAG = "com.greenpay.androidapp"; //log tag
+
+    BarcodeDetector barcodeDetector; //for detecting qr codes
+    CameraSource cameraSource; //for getting the images from the camera
+
+    //an id for the camera permission request
+    static final int camera_request = 1823;
+
+    TextView mainText1;
+    Button requestAccessBtn;
+
+    //an HTTP client to handle communication with server
+    OkHttpClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wifilist = findViewById(R.id.wifilist);
-        stateText = findViewById(R.id.stateText);
-        //get the wifi service and initialize the wifi manager
-        mainWiFi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        //set the wifiStateOriginal to the current state
-        //to turn off WiFi when leaving the app, in case it was off
-        wifiStateOriginal = mainWiFi.isWifiEnabled();
-        if (!mainWiFi.isWifiEnabled()) //if wifi is disabled, enabling wifi
+        mainText1 = findViewById(R.id.main_text1);
+        requestAccessBtn = findViewById(R.id.main_access_btn);
+        client = new OkHttpClient();
+        //set the barcode detector to detect QR codes
+        barcodeDetector = new BarcodeDetector.Builder(this).
+                setBarcodeFormats(Barcode.QR_CODE).
+                build();
+        cameraSource = new CameraSource.Builder(this, barcodeDetector).
+                setRequestedPreviewSize(640, 480).
+                build();
+        //set the listener for barcode detections
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>()
         {
-            stateText.setText("Enabling Wifi...");
-            mainWiFi.setWifiEnabled(true);
+            String lastBarcode = "";
+            @Override
+            public void release()
+            {
+
+            }
+
+            //this function is executed every time the app detects a barcode
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections)
+            {
+                //The array of the barcodes
+                SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() > 0)
+                {
+                    //The value of the barcode
+                    String barcodeValue = barcodes.valueAt(0).displayValue;
+                    if (barcodeValue.equals(lastBarcode))
+                        return;
+                    else
+                        lastBarcode = barcodeValue;
+                    Log.i(TAG, "Found barcode: " + barcodeValue);
+                    //check if the barcode belongs to GreenPay
+                    if (barcodeValue.startsWith("{greenpaycode}:"))
+                    {
+                        //the unique code of the barcode
+                        String code = barcodeValue.split(":")[1];
+                        //a http request to get barcode info
+                        Request request = new Request.Builder().
+                                url(HttpUrl.parse("http://greenpayserver.tk")
+                                .newBuilder()
+                                .addQueryParameter("request", "checkbarcode")
+                                .addQueryParameter("barcode", code)
+                                .build().toString()).build();
+                        //send the request
+                        client.newCall(request).enqueue(
+                                new Callback()
+                                {
+                                    @Override
+                                    public void onFailure(Call call, IOException e)
+                                    {
+                                        Log.e(TAG, "Failed to communicate with server");
+                                        Log.e(TAG, Log.getStackTraceString(e));
+                                    }
+
+                                    @Override
+                                    public void onResponse(Call call, Response response) throws IOException
+                                    {
+                                        if (!response.isSuccessful())
+                                        {
+                                            throw new IOException("Unexpected code " + response);
+                                        }
+                                        else
+                                        {
+
+                                        }
+                                    }
+                                }
+                        );
+                    }
+                }
+            }
+        });
+        //check if the application has permission to camera
+        if (checkCameraPermittion(MainActivity.this))
+        {
+            //for some reason, the camera work only after I stop it
+            cameraSource.stop();
+            //start the camera
+            startCamera();
+            //focus the camera
+            cameraFocus(cameraSource, Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+        } else
+            //request permission to camera
+            requestCameraPermission(MainActivity.this);
+    }
+
+    /**
+     * This function is used to focus the camera.
+     *
+     * @param cameraSource The camera source that will be focused
+     * @param focusMode    the focus mode
+     * @return true if the focus succeded
+     */
+    private static boolean cameraFocus(@NonNull CameraSource cameraSource, @NonNull String focusMode)
+    {
+        Field[] declaredFields = CameraSource.class.getDeclaredFields();
+
+        for (Field field : declaredFields)
+        {
+            if (field.getType() == Camera.class)
+            {
+                field.setAccessible(true);
+                try
+                {
+                    Camera camera = (Camera) field.get(cameraSource);
+                    if (camera != null)
+                    {
+                        Camera.Parameters params = camera.getParameters();
+                        params.setFocusMode(focusMode);
+                        camera.setParameters(params);
+                        return true;
+                    }
+
+                    return false;
+                } catch (IllegalAccessException e)
+                {
+                    e.printStackTrace();
+                }
+
+                break;
+            }
         }
-        stateText.setText("Wifi Enabled");
-        wifiReceiver = new BroadcastReceiver()
+
+        return false;
+    }
+
+    /**
+     * this function starts scanning the qr code from the camera
+     */
+    private void startCamera()
+    {
+        //change the text on the textview
+        mainText1.setText(R.string.point_camera);
+        try
         {
-            @Override
-            //this method is called every time a scan result is done
-            public void onReceive(Context context, Intent intent)
+            //start getting images from camera
+            cameraSource.start();
+        } catch (IOException | SecurityException e)
+        {
+            Log.e(TAG, Log.getStackTraceString(e));
+            mainText1.setText(R.string.error);
+            return;
+        }
+    }
+
+    /**
+     * This function checks if the app has permission to camera
+     *
+     * @param context the activity
+     * @return true if the app has access, false otherwise
+     */
+    public static boolean checkCameraPermittion(Context context)
+    {
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /**
+     * This function requests a permission to camera
+     *
+     * @param activity the activity which the function is called from
+     */
+    public static void requestCameraPermission(Activity activity)
+    {
+        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, camera_request);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if (requestCode == camera_request)
+        {
+            if (grantResults.length > 0)
             {
-                //check if the action was the wifi scan results
-                if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+                if (checkCameraPermittion(this))
                 {
-                    //the list of the wifi networks
-                    List<ScanResult> mScanResults = mainWiFi.getScanResults();
-                    stateText.setText("Found " + mScanResults.size() + " wifi networks");
-                    ArrayList <String> list = new ArrayList();
-                    for (int i = 0; i < mScanResults.size(); i++)
-                    {
-                        list.add(mScanResults.get(i).SSID);
-                    }
-                    //add all the wifi networks to the listview
-                    wifilist.setAdapter(new ArrayAdapter <String> (MainActivity.this,
-                            android.R.layout.simple_list_item_1, list));
-                    stateText.setText("wifi networks found");
+                    cameraSource.stop();
+                    startCamera();
+                } else
+                {
+                    mainText1.setText(R.string.camera_access_msg);
+                    requestAccessBtn.setVisibility(View.VISIBLE);
                 }
             }
-        };
-        //register the BroadcastReceiver
-        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        //a receiver for checking if the wifi state was changed
-        BroadcastReceiver onWifiStateChanged = new BroadcastReceiver()
-        {
-            @Override
-            public void onReceive(Context context, Intent intent)
-            {
-                if (intent.getAction().equals(WifiManager.WIFI_STATE_CHANGED_ACTION))
-                {
-                    if (mainWiFi.isWifiEnabled()) //if WiFi was off and now is enabled
-                    {
-                        //register the receiver
-                        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-                    }
-                    else //if wifi was on and now id disabled
-                    {
-                        //stop the receiver
-                        unregisterReceiver(wifiReceiver);
-                        //inform the user that the WiFi must be on
-                        Toast.makeText(context, "Service cannot work without WiFi", Toast.LENGTH_LONG).show();
-                        stateText.setText("WiFi is disabled");
-                    }
-                }
-            }
-        };
-        registerReceiver(onWifiStateChanged, new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
-        //start the wifi scan
-        mainWiFi.startScan();
-        stateText.setText("Scanning WiFi");
+        } else
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
 
-    protected void onPause() {
-        //stop scanning WiFi when leaving the app
-        unregisterReceiver(wifiReceiver);
-        //if the WiFi was off before the app was opened, turn it off
-        if (!wifiStateOriginal)
-            mainWiFi.setWifiEnabled(false);
-        //stop the app
+    public void requestCameraAccess(View view)
+    {
+        requestCameraPermission(this);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        cameraSource.stop();
         super.onPause();
     }
 
-    protected void onResume() {
-        //register the receiver back
-        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        mainWiFi.startScan(); //start a new scan
-        wifiStateOriginal = mainWiFi.isWifiEnabled(); //check if wifi is enabled
-        if (!wifiStateOriginal)
-            mainWiFi.setWifiEnabled(true); //enabling wifi if it was disabled
-        super.onResume(); //resume the app
+    @Override
+    protected void onRestart()
+    {
+        if (checkCameraPermittion(this))
+            startCamera();
+        super.onRestart();
     }
 }
